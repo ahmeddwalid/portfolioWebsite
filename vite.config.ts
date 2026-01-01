61b825348556b4720547ad278e579261c5ddc029
Ahmed Walid <devahmedwalid@proton.me>
2026-01-01 23:23:19 +0200

    update deployment to custom domain ahmeddwalid.me

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,16 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      // Add the base option here
-      base: '/portfolioWebsite/', // This is the line you need to add
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, '.', '');
+  return {
+    // Base path for custom domain
+    base: '/',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-      // If you have a 'plugins' array (e.g., for React or Vue), it would go here too.
-      // plugins: [react()], 
-    };
+    }
+    // If you have a 'plugins' array (e.g., for React or Vue), it would go here too.
+    // plugins: [react()], 
+  };
 });
